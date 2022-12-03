3946d0638a64baaaa4b105663a8381051591d0f5
Andre Arkhipov <andre.arhipow@gmail.com>
2022-12-03 18:42:24 +0300

    second commit

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,14 +1,16 @@
-window.onload = function() {
-	document.getElementById('modal_main').className = 'modal modal_active';
-	document.getElementsByClassName('show-success')[0].onclick = function() {
-		document.getElementById('modal_main').className = 'modal';
-		document.getElementById('modal_success').className = 'modal modal_active';
-	};
+const firstModal = document.getElementById('modal_main');
+firstModal.classList.toggle('modal_active');
+const secondModal = document.getElementById('modal_success');
+const closeModal = Array.from(document.getElementsByClassName('modal__close'));
+const successBtn = Array.from(document.getElementsByClassName('show-success'));
 
-	let arr = Array.from(document.getElementsByClassName('modal__close'));
-	for (let i = 0; i < arr.length; i++) {
-		arr[i].onclick = function(){
-			document.getElementsByClassName('modal_active')[0].className = 'modal';
-		};
-	}
+function changeModal() {
+    secondModal.classList.toggle('modal_active');
+    firstModal.classList.toggle('modal_active');
 }
+function close() {
+    this.closest('.modal_active').classList.toggle('modal_active');
+}
+
+closeModal.forEach(element => element.onclick = close);
+successBtn.forEach(element => element.onclick = changeModal);
